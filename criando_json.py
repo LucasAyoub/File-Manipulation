761cf2fa4f76a3c50af75979b2fc0efae4ed0e2c
Lucas L B Ayoub <74217723+LucasAyoub@users.noreply.github.com>
2020-12-11 19:52:04 -0300

    Update criando_json.py

--- criando_json.py
+++ criando_json.py
@@ -1,14 +1,20 @@
+#Importando json
 import json
+
+#Criando um dicionario
 dict = {"nome": "Tony Montana",
         "idade": "80",
         "linguagem": "Python"}
 
+#Abrimos um arquivo criado no programa e convertemos o conteudo para json
 with open("arquivos/produtos.json", "w") as arquivo:
     arquivo.write(json.dumps(dict))
 
+#Abrimos o arquivo para leitura após converter ele para json
 with open("arquivos/produtos.json", "r") as arquivo:
     leitura = arquivo.read()
     data = json.loads(leitura)
     print(leitura)
 
+#Imprimindo uma informação específica
 print(data["linguagem"])
