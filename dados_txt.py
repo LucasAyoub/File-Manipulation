feed7a1c23b011cb34182aec0b9eeb3ad51b7f3c
Lucas L B Ayoub <74217723+LucasAyoub@users.noreply.github.com>
2020-12-11 19:53:42 -0300

    Update dados_txt.py

--- dados_txt.py
+++ dados_txt.py
@@ -1,15 +1,19 @@
-#importando o os
+#Importando o os
 import os
+
+#Criando o texto
 texto = "Criando texto em Python "
 texto = texto + "e ainda adicionando conteúdo "
 texto += "ao texto"
 print(texto)
 
+#Mostrando o caminho do arquivo para escrita
 arquivo = open(os.path.join("arquivos/criacao.txt"), "w")
 for palavra in texto:
     arquivo.write(palavra + " ")
 arquivo.close()
 
+#Mostrando o caminho do arquivo para leitura
 arquivo = open("arquivos/criacao.txt", "r")
 conteudo = arquivo.read()
 arquivo.close()
