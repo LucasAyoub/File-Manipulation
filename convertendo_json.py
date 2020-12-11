03f51819783a18edb65b274a97774e73084ebb16
Lucas L B Ayoub <74217723+LucasAyoub@users.noreply.github.com>
2020-12-11 19:49:42 -0300

    Update convertendo_json.py

--- convertendo_json.py
+++ convertendo_json.py
@@ -1,9 +1,14 @@
+#Importamos json
 import json
+
+#Criando um dicionario
 dict = {"nome": "Tony Montana",
         "idade": "80",
         "linguagem": "Python"}
 
+#Printando os itens na tela e depois convertendo para json
 for x,y in dict.items():
     print(x,y)
 
 print(json.dumps(dict))
+
