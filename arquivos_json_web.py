d617eaef64943078c73fdcc98580fafe27e7ebd3
Lucas L B Ayoub <74217723+LucasAyoub@users.noreply.github.com>
2020-12-11 19:47:56 -0300

    Update arquivos_json_web.py

--- arquivos_json_web.py
+++ arquivos_json_web.py
@@ -1,13 +1,14 @@
+#Importação
 from urllib.request import urlopen
 import json
 
+#Foi colocado a URL do arquivo que desejamos usar
 arquivo = urlopen("http://vimeo.com/api/v2/video/57733101.json").read().decode("utf8")
 leitura = json.loads(arquivo)[0]
 
+#De acordo com o arquivo colocamos as palavras chaves para obter os dados do mesmo
 print ('Título: ', leitura['title'])
 print ('URL: ', leitura['url'])
 print ('Duração: ', leitura['duration'])
 print ('Número de Visualizações: ', leitura['stats_number_of_plays'])
-
-
 
