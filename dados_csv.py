975e019393497574ac7d4032179387a6e605415b
Lucas L B Ayoub <74217723+LucasAyoub@users.noreply.github.com>
2020-12-11 19:52:48 -0300

    Update dados_csv.py

--- dados_csv.py
+++ dados_csv.py
@@ -1,10 +1,13 @@
+#Importando o csv
 import csv
 
+#Abrindo o arquivo com with para escrita
 with open("arquivos/numeros.csv", "w") as arquivo:
     escrita = csv.writer(arquivo)
     escrita.writerow(("abacaxi", "uva", "pera"))
     escrita.writerow((12, 47, 68))
 
+#Para evitar linhas em branco, usamos o encoding para leitura e depois imprimimos também o número de colunas
 with open('arquivos/numeros.csv','r', encoding='utf8', newline = '\r\n') as arquivo:
     leitor = csv.reader(arquivo)
     for x in leitor:
